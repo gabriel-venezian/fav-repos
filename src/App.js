20c820495b4a3a046b8df40e12d7ed256665bdca
Gabriel Venezian <gabriel.venezian@hotmail.com>
2022-12-13 20:06:59 -0300

    Add global style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
 import Router from './router';
+import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
-    <Router />
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Router />
+    </>
   );
 }
